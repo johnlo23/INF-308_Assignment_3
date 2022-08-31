ac90b716e19e99828b05d32993e2cf2fcd0faacf
johnlo23 <john@jlny.org>
2022-08-31 18:29:17 -0400

    Starter code from professor.

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+file = open('text.txt', 'w')
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+s = "Hello there\n"
 
+file.write(s)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+file.close()
 
+file = open('text.txt', 'r')
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+print(file.read())
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+file.close()
