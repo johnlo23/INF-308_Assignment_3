25ad9a7b81d7d3c8d36e6bc79d623280895451fc
John <john@jlny.org>
2022-09-05 22:59:29 -0400

    Word is supplied by user. Added comments.

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
+# Open a file for writing
 file = open('text.txt', 'w')
 
-s = "Hello there\n"
+# Get a word from the user
+word = input("Please enter one word: ")
 
-file.write(s)
+# Write the word to a file with a trailing newline character
+file.write(word + "\n")
 
 file.close()
 
+# Open the file for reading
 file = open('text.txt', 'r')
 
-print(file.read())
+# Read the contents of the file
+file_word = file.read()
+
+print(f"You entered and save the word {file_word}")
 
 file.close()
